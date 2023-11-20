767cd6a0dfea0b214eaecf528cb915613d02a9f0
Sonal4y <sonal4y@gmail.com>
2023-11-20 10:56:25 +0530

    initial commit

---
+++ LabProject/Sum.java
@@ -0,0 +1,15 @@
+package Day1;
+//wap for addition of two number
+public class Sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//local variable
+		int a = 22;
+		int b = 12;
+		int c = a+b;
+	
+		System.out.append("Sum = "+c);
+	}
+
+}
